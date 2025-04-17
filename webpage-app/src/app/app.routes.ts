9c2bf0464527c0b906a525ff15cad18cc4d6f29f
Edwin-Antonio~ <edanarellanoma@ittepic.edu.mx>
2025-04-16 19:11:52 -0600

    New components and html implemented

--- webpage-app/src/app/app.routes.ts
+++ webpage-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    // component: DashboardPageComponent  <---- Esta es otra manera más sencilla de importar un componente pero un poco más pesado en recursos
+    // el LoadComponent funciona igual solmanete que es un poco más rapido
+    loadComponent: () =>
+      import('./webpage/pages/dashborad-page/dashboard-page.component')
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
